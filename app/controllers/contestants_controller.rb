669653a3fc933752f11a8a2f99940c782402c93e
Steve Roettger <steve@steveroettger.com>
2012-08-29 14:01:56 -0400

    completed contestant new/create

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,9 +1,16 @@
 class ContestantsController < ApplicationController
   def new
     #create a new contestant by entering name + email
+    @contestant = Contestant.new
   end
   
   def create 
     #create a new contestant by entering name + email
+    @contestant = Contestant.new(params[:contestant])
+    if @contestant.save
+      redirect_to @contestant
+    else
+      render 'new'
+    end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def main
     app_secret = 'add3ad768bdb7760b76721943f393814'
     @signed_request = FBGraph::Canvas.parse_signed_request(app_secret, params[:signed_request])
+    @contestant = Contestant.new
   end
 end

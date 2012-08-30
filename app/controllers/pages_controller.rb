75fe42cd31f5af41c4f6c5e6a368bfbc77f66b19
Steve Roettger <steve@steveroettger.com>
2012-08-30 09:39:50 -0400

    updated fangate functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def main
-    app_secret = 'add3ad768bdb7760b76721943f393814'
-    @signed_request = FBGraph::Canvas.parse_signed_request(app_secret, params[:signed_request])
+    #app_secret = 'add3ad768bdb7760b76721943f393814'
+    #@signed_request = FBGraph::Canvas.parse_signed_request(app_secret, params[:signed_request])
     @contestant = Contestant.new
   end
 end

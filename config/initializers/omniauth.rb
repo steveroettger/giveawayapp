0e0c751b8a08af76337509169c0b7c7d14d1d6b6
Steve Roettger <steve@steveroettger.com>
2012-10-02 20:02:31 -0400

    adding js test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['461550273890419'], ENV['add3ad768bdb7760b76721943f393814']
+  provider :facebook, '461550273890419', 'add3ad768bdb7760b76721943f393814'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 GiveawayApp::Application.routes.draw do
   root to: 'pages#main'
+  match 'test', to: 'pages#test'
   resources :contestants
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')

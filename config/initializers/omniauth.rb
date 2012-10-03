c7bfb05a5c85a0b4cafce29031d9fdb613cd442e
Steve Roettger <steve@steveroettger.com>
2012-10-02 20:28:00 -0400

    testing fb login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '461550273890419', 'add3ad768bdb7760b76721943f393814', 
   :iframe => true,
-  :callback_url => "http://apps.facebook.com/app_461550273890419/auth/facebook/callback"
+  :callback_url => "https://apps.facebook.com/fiatgiveaway//auth/facebook/callback"
 end
 
 

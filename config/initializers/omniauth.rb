fce906d30c398262f47f271fd89b1f930d5d095c
Steve Roettger <steve@steveroettger.com>
2012-10-02 21:50:17 -0400

    testing with popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '461550273890419', 'add3ad768bdb7760b76721943f393814'
+  provider :facebook, '461550273890419', 'add3ad768bdb7760b76721943f393814', :display => 'popup'
 end
 
 

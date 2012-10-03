2a975b494a68af042d5d7482710a47ba1f198afa
Steve Roettger <steve@steveroettger.com>
2012-10-02 20:20:42 -0400

    testing popup auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '461550273890419', 'add3ad768bdb7760b76721943f393814'
+  provider :facebook, '461550273890419', 'add3ad768bdb7760b76721943f393814', 
+  :iframe => true,
+  :callback_url => "http://apps.facebook.com/app_461550273890419/auth/facebook/callback"
 end
+
+
